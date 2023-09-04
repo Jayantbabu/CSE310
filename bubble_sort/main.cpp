5c803df0a72f75a77527642c789eed1314e7df28
Jayantbabu <49016017+Jayantbabu@users.noreply.github.com>
2023-09-04 23:17:02 +0000

    bubble

--- bubble_sort/main.cpp
+++ bubble_sort/main.cpp
@@ -4,7 +4,6 @@ void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -15,15 +14,15 @@ void bubbleSort(int arr[], int n) {
 
 int main() {
     int n;
-    std::cin >> n; // Input number of elements
+    std::cin >> n; 
     int arr[n];
     for (int i = 0; i < n; i++) {
-        std::cin >> arr[i]; // Input array elements
+        std::cin >> arr[i]; 
     }
 
     bubbleSort(arr, n);
 
-    // Output sorted array
+
     for (int i = 0; i < n; i++) {
         std::cout << arr[i] << " ";
     }
